9f329112930821d2c2451b43ca88be2a1dc8836f
Sebastian Weyer <sebastian.weyer@canonical.com>
2026-06-18 12:58:25 +0000

    WIP update to 39.2

--- cuda-samples/scripts/bin/execute_from_target_dir.sh
+++ cuda-samples/scripts/bin/execute_from_target_dir.sh
@@ -6,5 +6,7 @@ CMD=$SNAP/usr/bin/$1
 TARGET=$(readlink -f $CMD)
 TARGET_DIR=$(dirname $TARGET)
 
+export LD_LIBRARY_PATH=$SNAP/graphics/opt/nvidia/l4t-gpu-libs/openrm:$LD_LIBRARY_PATH
+
 cd $TARGET_DIR
 $(basename $CMD)
