e9c9e2f83d04c197634476afa2e2c00601e206db
Sebastian Weyer <sebastian.weyer@canonical.com>
2026-06-19 12:14:43 +0000

    wip update to rel39.2

--- cuda-samples/scripts/bin/execute_from_target_dir.sh
+++ cuda-samples/scripts/bin/execute_from_target_dir.sh
@@ -6,7 +6,17 @@ CMD=$SNAP/usr/bin/$1
 TARGET=$(readlink -f $CMD)
 TARGET_DIR=$(dirname $TARGET)
 
+shift 1
+
 export LD_LIBRARY_PATH=$SNAP/graphics/opt/nvidia/l4t-gpu-libs/openrm:$LD_LIBRARY_PATH
 
+EXEC=$(basename $CMD)
+
+if [ -n "$1" ]; then
+        EXTRA_ARGS=$@
+elif [ $EXEC == "ptxgen" ]; then
+        EXTRA_ARGS="test.ll"
+fi
+
 cd $TARGET_DIR
-$(basename $CMD)
+$EXEC $EXTRA_ARGS
