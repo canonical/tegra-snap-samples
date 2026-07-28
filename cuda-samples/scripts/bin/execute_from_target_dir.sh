25b16da7330029d13830c2fd18899e005553ee75
Sebastian Weyer <sebastian.weyer@canonical.com>
2026-07-28 08:16:24 +0000

    Update all snaps to core24 and rel39 base
    Update all snaps to compile and run with core24 based packages and the
    rel39 package repositories from nvidia.
    
    Adapt all build steps to compile the new sources and adapt paths of all
    provider wrappers.
    
    Add missing interfaces for snaps to successfully run.
    
    Signed-off-by: Sebastian Weyer <sebastian.weyer@canonical.com>

--- cuda-samples/scripts/bin/execute_from_target_dir.sh
+++ cuda-samples/scripts/bin/execute_from_target_dir.sh
@@ -6,5 +6,15 @@ CMD=$SNAP/usr/bin/$1
 TARGET=$(readlink -f $CMD)
 TARGET_DIR=$(dirname $TARGET)
 
+shift 1
+
+EXEC=$(basename $CMD)
+
+if [ -n "$1" ]; then
+        EXTRA_ARGS=$@
+elif [ $EXEC == "ptxgen" ]; then
+        EXTRA_ARGS="test.ll"
+fi
+
 cd $TARGET_DIR
-$(basename $CMD)
+$EXEC $EXTRA_ARGS
